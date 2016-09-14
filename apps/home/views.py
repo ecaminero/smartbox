6a28d83bf740f96e4a9ee91e7adfff868c132f66
ecaminero <caminero.edwin@gmail.com>
2016-09-13 21:57:58 -0300

    Agregando funcionalidad de tiempo de ejeccion de funcion

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import json, datetime
+import json, datetime, time
 from django.shortcuts import render
 from django.http import JsonResponse
 
-
+# Decorators
 def json_converter(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
@@ -12,20 +12,25 @@ def json_converter(func):
 
 def datetime_decorator(func):
     def wrapper(*args, **kwargs):
-        now = datetime.datetime.now()
         result = func(*args, **kwargs)
+        now = datetime.datetime.now()
         result['datetime'] = now.isoformat()
         return result
     return wrapper
 
+def execution_time(func):
+    def wrapper(*args):
+        time1, result, time2 = time.time(), func(*args), time.time()
+        result['completed_in'] = round((time2-time1)*1000.0, 4)
+        return result
+    return wrapper
 
 # Create your views here.
 @json_converter
+@execution_time
 @datetime_decorator
 def index(request):
-    completed_in = 0;
     response = {
-      "completed_in": completed_in,
       "c": { "d": "e" },
       "a": "b" 
     }
